3a20eec40f3d9b8e6cb04344a740b14a0ee5b269
lolu1023 <soowkw@naver.com>
2021-12-07 21:06:44 +0900

    성공

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
-let tab_head = document.querySelector('.tab>ul');
-let on = document.querySelector('.tab>li');
-tab_head.addEventListener('click',function () {
-    on.classList.add('is_on')
-});
+const tab_head = document.querySelectorAll('.tab li');
+const contents = document.querySelectorAll('.content ul');
+// console.log(contents);
+// console.log(tab_head);
+for(let i = 0; i < tab_head.length; i++) {
+    tab_head[i].addEventListener('click', (e)=> {
+        e.preventDefault()
+        for(let j = 0; j < tab_head.length; j++) {
+            tab_head[j].classList.remove('active')
+            contents[j].classList.remove('active')
+        }
+        tab_head[i].classList.add('active')
+        contents[i].classList.add('active')
+    });
+};
+// if(i === 1) {
+//     tab_head[0].classList.remove('active');
+//     tab_head[1].classList.add('active');
+//     tab_head[2].classList.remove('active');
+//     contents[0].classList.remove('active');
+//     contents[1].classList.add('active');
+//     contents[2].classList.remove('active');
+// }else if(i === 2) {
+//     tab_head[0].classList.remove('active');
+//     tab_head[1].classList.remove('active');
+//     tab_head[2].classList.add('active')
+//     contents[0].classList.remove('active');
+//     contents[1].classList.remove('active');
+//     contents[2].classList.add('active')
+// }else {
+//     tab_head[0].classList.add('active')
+//     tab_head[1].classList.remove('active');
+//     tab_head[2].classList.remove('active');
+//     contents[0].classList.add('active')
+//     contents[1].classList.remove('active');
+//     contents[2].classList.remove('active');
+// }
